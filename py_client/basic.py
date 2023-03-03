24bd50adc98ca5725ec0a184d6f1c9c8337641fa
108806 <1088xvx@gmail.com>
2023-03-03 10:18:17 +0100

    py_client tests

--- py_client/basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-from rich import print as pprint
-
-endpoint = 'http://127.0.0.1:8000/api/'
-
-with requests.Session() as s:
-    res = s.get(endpoint, json={'product_id':'123'})
-    pprint(res.json())
