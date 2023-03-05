720f3b65923348475dcdc983dca8e37a64fed5f5
Winston Smith <1088xvx@gmail.com>
2023-03-05 11:05:11 +0100

    heroku test v4

--- core/settings.py
+++ core/settings.py
@@ -15,8 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import os, django_heroku.settings, dj_database_url,
-
+import os, django_heroku, dj_database_url
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
