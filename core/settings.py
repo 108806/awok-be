9714effc78e6f095de31df9dcf39aba678ecd183
Winston Smith <1088xvx@gmail.com>
2023-03-07 01:01:00 +0100

    heroku cors v8

--- core/settings.py
+++ core/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
 #CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = [
-    'https://antoniwrobel.github.aio',
+    'https://antoniwrobel.github.io',
     'https://localhost',
     'https://127.0.0.1',
     'https://vps-1a79e2b1.vps.ovh.net'
